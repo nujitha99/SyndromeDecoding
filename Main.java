deaa2fe73fd39a5f221ef0c12b30e8697f264408
Nujitha Wickramasurendra <nwickramasurendra@gmail.com>
2023-11-20 19:33:41 -0500

    Initial commit

--- Main.java
+++ Main.java
@@ -1,91 +1,127 @@
-import java.util.ArrayList;
-import java.util.List;
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.*;
 
 public class Main {
+    ArrayList<String> cosetLeaders = new ArrayList<>(); // stores all the coset leaders
 
-    // Function to calculate the edit distance between two bit strings
-    private static int editDistance(int[] s1, int[] s2) {
-        int m = s1.length;
-        int n = s2.length;
+    TreeMap<String, String> syndromeMap = new TreeMap<>(); // stores the syndrome map
 
-        int[][] dp = new int[m + 1][n + 1];
-
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0) {
-                    dp[i][j] = j;
-                } else if (j == 0) {
-                    dp[i][j] = i;
-                } else if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1]));
-                }
-            }
-        }
-
-        return dp[m][n];
-    }
-
-    // Function to check if a set of codewords forms an optimal edit metric error-correcting code
-    private static boolean isOptimalEditMetricCode(List<int[]> codewords, int n, int d, int q, int M) {
-        for (int i = 0; i < codewords.size(); i++) {
-            for (int j = i + 1; j < codewords.size(); j++) {
-                int distance = editDistance(codewords.get(i), codewords.get(j));
-                if (distance < d) {
-                    System.out.println(distance);
-                    return false; // Codewords violate the minimum distance condition
-                }
-            }
-        }
-
-        return true; // All pairs of codewords satisfy the minimum distance condition
-    }
-
-    // Function for exhaustive search to find an optimal edit metric error-correcting code with more than M codewords
-    public static boolean exhaustiveSearch(int n, int d, int q, int M) {
-        int qToN = (int) Math.pow(q, n);
-
-        for (int i = M + 1; i <= qToN; i++) {
-            List<int[]> codewords = generateAllCodewords(n, i, q);
-            if (isOptimalEditMetricCode(codewords, n, d, q, M)) {
-                return false; // An optimal edit metric code with more than M codewords is found
-            }
-        }
-
-        return true; // No optimal edit metric code with more than M codewords is found
-    }
-
-    // Function to generate all possible codewords of length n and alphabet size q
-    private static List<int[]> generateAllCodewords(int n, int M, int q) {
-        List<int[]> codewords = new ArrayList<>();
-        generateCodewords(new int[n], 0, n, M, q, codewords);
-        return codewords;
-    }
-
-    private static void generateCodewords(int[] current, int index, int n, int M, int q, List<int[]> codewords) {
-        if (index == n) {
-            codewords.add(current.clone());
-        } else {
-            for (int i = 0; i < q; i++) {
-                current[index] = i;
-                generateCodewords(current, index + 1, n, M, q, codewords);
-            }
-        }
-    }
+    // parity check matrix
+    int[][] pcMatrix =
+            {
+                    {1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0},
+                    {1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0},
+                    {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0},
+                    {0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0},
+                    {0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0},
+                    {0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1}
+            };
 
     public static void main(String[] args) {
-        int n = 2;  // replace with your desired value
-        int d = 4;  // replace with your desired value
-        int q = 2;  // replace with your desired value
-        int M = 1; // replace with your desired value
+        Main main = new Main();
 
-        boolean codeExists = exhaustiveSearch(n, d, q, M);
+        // find the syndrome table
+        main.generateCosetLeaders(12);
+        main.calcSyndrome();
 
-        if (!codeExists) {
-            System.out.println("An optimal edit metric error-correcting code with more than " + M + " codewords exists.");
-        } else {
-            System.out.println("No optimal edit metric error-correcting code with more than " + M + " codewords exists.");
-        }
+        // execute syndrome decoding for the given values
+        main.syndromeDecode("111111111111");
+        main.syndromeDecode("000011100000");
+        main.syndromeDecode("001100110000");
     }
+
+    private void generateCosetLeaders(int pow) {
+
+        int total = (int) Math.pow(2, pow);
+        for (int i = 0; i < total; i++) { // generates all the coset leaders from 1 to the given power 12
+            String result = Integer.toBinaryString(i);
+            String resultFormatted = String.format("%12s", result).replaceAll(" ", "0");
+            cosetLeaders.add(resultFormatted);
+        }
+
+    }
+
+
+    private void calcSyndrome() {
+        int weight = 0;
+
+        ArrayList<String> syndromeList = new ArrayList<>();
+        // iterate for all 12 weights
+        for (int i = 0; i <= 12; i++) {
+            ArrayList<String> filteredCosetLeaders = new ArrayList<>();
+            // filter coset leaders starting weight 0
+            for (String bnry : cosetLeaders) {
+                if (bnry.chars().filter(ch -> ch == '1').count() == weight) {
+                    filteredCosetLeaders.add(bnry);
+                }
+            }
+            // iterate for each coset leader in the filtered list
+            for (String cosetLeader : filteredCosetLeaders) {
+                StringBuilder syndrome = new StringBuilder(" ");
+                // take each vector in pc matrix
+                for (int[] matrix : pcMatrix) {
+                    int vectorMultSum = 0;
+                    // multiply bits of the vector with the bits in current coset leader in order
+                    for (int k = 0; k < matrix.length; k++) {
+                        char chr = cosetLeader.charAt(k);
+                        vectorMultSum += matrix[k] * Integer.parseInt(String.valueOf(chr));
+                    }
+                    syndrome.append(String.valueOf(vectorMultSum % 2));
+                }
+                // save the calculated syndrome with coset leader if the syndrome does not exist
+                if (!syndromeList.contains(syndrome.toString())) {
+                    syndromeList.add(syndrome.toString());
+                    syndromeMap.put(cosetLeader, String.valueOf(syndrome));
+                }
+            }
+            weight++;
+
+        }
+
+        // print syndrome table
+        System.out.println(" Coset Leader  |  Syndrome ");
+        System.out.println("---------------------------");
+        for (Map.Entry<String, String> entry : syndromeMap.entrySet()) {
+            System.out.println(" " + entry.getKey() + "  |  " + entry.getValue());
+        }
+        System.out.println("Table size: " + syndromeMap.size());
+    }
+
+    private void syndromeDecode(String received) {
+        StringBuilder syn = new StringBuilder(" ");
+        String cosetLeaderE = "";
+        StringBuilder decodedResult = new StringBuilder(" ");
+        // find the syn of received vector
+        for (int[] matrix : pcMatrix) {
+            int vectorMultSum = 0;
+            // multiply bits of the vector with the bits in received word in order
+            for (int k = 0; k < matrix.length; k++) {
+                char chr = received.charAt(k);
+                vectorMultSum += matrix[k] * Integer.parseInt(String.valueOf(chr));
+            }
+            syn.append(String.valueOf(vectorMultSum % 2));
+        }
+
+        // find the matching coset leader 'e' from syndrome table map
+        for(Map.Entry<String, String> entry: syndromeMap.entrySet()) { 
+            if(Objects.equals(entry.getValue(), String.valueOf(syn))) {
+                cosetLeaderE = entry.getKey();
+                break;
+            }
+        }
+        //find the decoding result by subtracting coset leader from received word
+        for (int i = 0; i < cosetLeaderE.length(); i++) {
+            int y = Integer.parseInt(String.valueOf(received.charAt(i)));
+            int e = Integer.parseInt(String.valueOf(cosetLeaderE.charAt(i)));
+            decodedResult.append(String.valueOf(Math.abs(y - e)));
+        }
+
+        // print the decoding result summary
+        System.out.println();
+        System.out.println("Y = " + received + " | E = " + cosetLeaderE + " | Syndrome = " + syn);
+        System.out.println("Decoded " + received + " = " + decodedResult);
+
+    }
+
 }
